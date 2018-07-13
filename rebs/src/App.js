51a28b360752ca931536ff682c1e041d5e8650a8
Cameo <hello@aeglir.com.au>
2018-07-13 17:52:42 +1000

    Add dropdown menu

--- rebs/src/App.js
+++ rebs/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import './App.css';
 
 // IMPORT COMPONENTS
 import Navbar from './components/Navbar/Navbar'
 
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
+
         <BrowserRouter>
           <Navbar/>
         </BrowserRouter>
--- rebs/src/components/Navbar/Navbar.js
+++ rebs/src/components/Navbar/Navbar.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
+import Popup from "reactjs-popup";
 import './Navbar.css';
+
+
 
 class Navbar extends Component {
   render() {
+
     return (
-      <div className="Navbar">
-    <Link to="/events"><FontAwesome name="calendar-alt"/></Link>
-    <Link to="/facilitators"><FontAwesome name="users"/></Link>
-    <Link to="/events/new" id="blue-circle"><FontAwesome name="plus"/></Link>
-    <Link to="/organisations"><FontAwesome name="university"/></Link>
-    <Link to="/settings"><FontAwesome name="cog"/></Link>
-      </div>
+        <div className="Navbar">
+
+        {/* Events Link with Popup */}
+            <Popup
+            trigger={<p><FontAwesome name="calendar-alt"/></p>}
+            position="top left"
+            on="click"
+            closeOnDocumentClick
+            mouseLeaveDelay={300}
+            mouseEnterDelay={0}
+            contentStyle={{ padding: "0px", border: "none" }}
+            arrow={false}
+            >
+            <div className="menu">
+                <Link to="/events"><p>View All</p></Link>
+                <Link to="/events/new"><p>New Event</p></Link>
+            </div>
+            </Popup>
+
+        {/* Facilitators Link with Popup */}
+            <Popup
+            trigger={<p><FontAwesome name="users"/></p>}
+            position="top left"
+            on="click"
+            closeOnDocumentClick
+            mouseLeaveDelay={300}
+            mouseEnterDelay={0}
+            contentStyle={{ padding: "0px", border: "none" }}
+            arrow={false}
+            >
+            <div className="menu">
+                <Link to="/facilitators"><p>View All</p></Link>
+                <Link to="/facilitators/new"><p>New Facilitator</p></Link>
+            </div>
+            </Popup>
+
+        {/* New Event Link */}
+        <Link to="/events/new" id="blue-circle"><FontAwesome name="plus"/></Link>
+
+        {/* Organisations Link with Popup */}
+            <Popup
+            trigger={<p><FontAwesome name="university"/></p>}
+            position="top right"
+            on="click"
+            closeOnDocumentClick
+            mouseLeaveDelay={300}
+            mouseEnterDelay={0}
+            contentStyle={{ padding: "0px", border: "none" }}
+            arrow={false}
+            >
+            <div className="menu">
+                <Link to="/orginisations"><p>View All</p></Link>
+                <Link to="/orginisations/new"><p>New Organisation</p></Link>
+            </div>
+            </Popup>
+
+        {/* Settings Link */}
+        <Link to="/settings"><FontAwesome name="cog"/></Link>
+
+        </div>
     );
   }
 }
