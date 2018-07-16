367f77c6e01a8e04a0aa8bdc34f7b2b35499f163
Steven Christenson <stevenchristenson@Stevens-MacBook-Air.local>
2018-07-16 15:18:08 +1000

    prep for dynamic edit workshop form

--- rebs/src/components/Events/EventEdit.js
+++ rebs/src/components/Events/EventEdit.js
@@ -2,6 +2,8 @@ import React from 'react'
 import  './EventEdit.css'
 
 
+
+
 function EventEdit() {
   return (
     <div className="eventEdit">
@@ -9,11 +11,11 @@ function EventEdit() {
         <form onSubmit={(e) =>{
           e.preventDefault()
         }}>
-          <input type="text" placeholder="   workshop" name="workshop" /><br/>
-          <input type="text" placeholder="   facilitator" name="facilitator" /><br/>
-          <button placeholder="Add Facilitator" className="AddFacilitator"></button>
+          <input type="text" placeholder="   Workshop" name="workshop" required/><br/>
+          <input type="text" placeholder="   Facilitator" name="facilitator" required/><br/>
+          <button placeholder="Add Facilitator" className="AddFacilitator" required></button>
           <div className="">
-            <input type="button" value="Add Facilitator" name="Add Facilitator" /><br />
+            <input type="button" value="Add Facilitator" name="addFacilitator" required/><br />
           </div>
             
             <div className="onsite">
@@ -21,16 +23,17 @@ function EventEdit() {
               <p>Onsite</p>
             </div>
 
-          <input type="text" placeholder="   Organization" name="organization" /><br/>
-          <input type="text" placeholder="   location" name="location" /><br/>
-          <input type="text" placeholder="   Notes" name="Notes" /><br />
+          <input type="text" placeholder="   Organization" name="organization" required/><br/>
+          <input type="text" placeholder="   Location" name="location" required/><br/>
+          <input type="text" placeholder="   Notes" name="Notes" required/><br />
 
           <label>Start Date</label>
           <label>End Date</label><br />
           <div className="dates">
-            <input type="datetime-local" value="Start Date" name="startDate" />             {/*TODO: Check datetime input*/}
-            <input type="datetime-local" value="End Date" name="endDate" />                 {/*TODO: Check datetime input*/}
+            <input type="datetime-local" value="Start Date" name="startDate" required/>             {/*TODO: Check datetime input*/}
+            <input type="datetime-local" value="End Date" name="endDate" required/>              {/*TODO: Check datetime input*/}
           </div>
+         
 
           <input type="button" value="Add Another Date" name="addEvent" /><br />
 
