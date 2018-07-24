83b82372edc614e9b905097062c15de16182785a
Cameo <hello@aeglir.com.au>
2018-07-24 11:30:27 +1000

    Fixing up EventEdit

--- rebs/src/components/Events/EventEdit.js
+++ rebs/src/components/Events/EventEdit.js
@@ -19,6 +19,9 @@ class EventEdit extends Component {
     selectedLocation: '',
     selectedOrganisation: '',
     selectedStatus: '',
+    users: null,
+    orgs: null,
+    workshops: null,
   }
   
 
@@ -51,23 +54,7 @@ class EventEdit extends Component {
           "end": new Date(this.state.endDate),
           "location": this.state.selectedLocation,
           }]
-      }
-        
-        {
-        _id: workshop_id,
-      title: this.state.selectedWorkshop,
-      organisation: this.state.selectedOrganisation,
-      notes: e.target.notes.value,
-      facilitators:  this.state.selectedFacilitators,
-      attendees: e.target.attendees.value,
-      onsite: e.target.onsite.checked,
-      status: this.state.selectedStatus,
-      bookings: [{
-        location: this.state.selectedLocation,
-        start: "2018-08-04T06:30:00.000Z",
-        end: "2018-08-08T06:00:00.000Z"
-      }],
-    })
+      })
     .then((res) => {
       console.log(res.data.title);
       console.log(res.data);
@@ -88,26 +75,30 @@ class EventEdit extends Component {
     }
 
     workshopSelect = (workshop) => {
-      this.setState({ selectedWorkshop: workshop});
+      console.log("workshop")
+      console.log(workshop)
+      // this.setState({ selectedWorkshop: workshop.value});
     }
-    
     facilitatorSelect = (facilitator) => {
-      this.setState({ selectedFacilitator: facilitator });
-    }
-    
-    facilitatorSelect = (facilitator) => {
+      console.log("facilitator")
+      console.log(facilitator)
       this.setState({ selectedFacilitator: facilitator })
       }
 
-      locationSelect = (location) => {
-        this.setState({ selectedLocation: location });
-        }
+    locationSelect = (location) => {
+      console.log("location")
+      console.log(location)
+      this.setState({ selectedLocation: location });
+      }
 
     organisationSelect = (organisation) => {
+      console.log("organisation")
+      console.log(organisation)
       this.setState({ selectedOrganisation: organisation})
     }
-
     statusSelect = (status) => {
+      console.log("status")
+      console.log(status)
       this.setState({ selectedStatus: status})
     }
 
@@ -122,12 +113,29 @@ class EventEdit extends Component {
         selectedOrganisation: {value: singleEvent.organisation, label: singleEvent.organisation},
         selectedStatus: {value: singleEvent.status, label: singleEvent.status}
       })
+      axios.get(`${process.env.REACT_APP_API_URI}/users`)
+      .then(users => {
+        this.setState({users: users.data})
+        console.log(users.data)
+      })
+
+      axios.get(`${process.env.REACT_APP_API_URI}/organisations`)
+      .then(orgs => {
+        this.setState({orgs: orgs.data})
+        console.log(orgs.data)
+      })
+
+      axios.get(`${process.env.REACT_APP_API_URI}/workshops`)
+      .then(workshops => {
+        this.setState({workshops: workshops.data})
+        console.log(workshops.data)
+      })
     }
 
 
     render() {
       const singleEvent = this.props.location.state.singleEvent
-      const { startDate, endDate, redirect, selectedStatus, selectedWorkshop, selectedFacilitator, selectedLocation, selectedOrganisation } = this.state;
+      const { startDate, endDate, redirect, selectedStatus, selectedWorkshop, selectedFacilitator, selectedLocation, selectedOrganisation, orgs, users, workshops } = this.state;
 
 
 
@@ -182,6 +190,9 @@ class EventEdit extends Component {
         state: { singleEvent: singleEvent }
       }}/>
     }
+    if(!users || !orgs || !workshops){
+      return <h2>Loading...</h2>
+    }
     return (
       <div className="eventEdit">
         <h2>Edit Workshop</h2><br/><br/>
@@ -190,66 +201,30 @@ class EventEdit extends Component {
             this.handleChange(e)
             // console.log(this.input);
           }}>
-            
-              
+                         
             <SingleSelect
               name="workshopTitle"
-              simpleValue
-              value={selectedWorkshop}
+              // value={selectedWorkshop}
               onChange={this.workshopSelect}
               required
-              options={[
-                { value: 'Hands-on Coding for Beginners', label: 'Hands-on Coding for Beginners' },
-                      { value: 'Hands-on Coding for Intermediate', label: 'Hands-on Coding for Intermediate' },
-                      { value: 'Unity Gamemaker for Kids', label: 'Unity Gamemaker for Kids' },
-                      { value: 'Build a Web App (HTML, CSS, JavaScript', label: 'Build a Web App (HTML, CSS, JavaScript) ' },
-                      { value: 'IoT and Ardunio workshop ', label: 'IoT and Ardunio workshop ' },
-                      { value: 'Coding & Robotics for Kids', label: 'Coding & Robotics for Kids' },
-                      { value: 'userSchool Excursion - Intro to JavascriptID1', label: 'School Excursion - Intro to Javascript ' },
-                      { value: 'Unity Gamemaker for Kids Day 3 of 3', label: 'Unity Gamemaker for Kids Day 3 of 3' },
-                      { value: 'Become a Digital Artist', label: 'Become a Digital Artist' },
-                      { value: 'userCreate VFX in FilmID1', label: 'Create VFX in Film' },
-                      { value: 'Code Your World', label: 'Code Your World' },
-                      { value: 'Immersive Robotocs', label: 'Immersive Robotocs' },
-                      { value: 'Gamers Unite', label: 'Gamers Unite' },
-                      { value: '3D Character Creation', label: '3D Character Creation ' },
-                      { value: 'Virtual Reality (VR) Experience', label: 'Virtual Reality (VR) Experience' }
-              ]}
+              options={workshops.map(workshop => {
+                return {value: workshop._id, label: workshop.workshop_name}
+              })}
             />
           
-          
-                <MultiSelect
-                  multi
-                  joinValues
-                  delimiter={','}
-                  name="facilitators"
-                  value={selectedFacilitator}
-                  onChange={this.facilitatorSelect}
-                  required
-                  options={[
-                    { value: 'userID1', label: 'Amos Jon Wilksch' },
-                    { value: 'userID2', label: 'Annabelle (Bella) Maguire' },
-                    { value: 'userID3', label: 'Andrew Madden' },
-                    { value: 'userID4', label: 'Arian Yusef' },
-                    { value: 'userID5', label: 'Billy' },
-                    { value: 'userID6', label: 'Bushra Malik' },
-                    { value: 'userID7', label: 'Camilla Stadlinger' },
-                    { value: 'userID8', label: 'Christian Saviane' },
-                    { value: 'userID9', label: 'Jacqueline tate ' },
-                    { value: 'userID10', label: 'Kelsey Birrel' },
-                    { value: 'userID11', label: 'Lani Gambino' },
-                    { value: 'userID12', label: 'Marc Pestamento' },
-                    { value: 'userID13', label: 'Najat Smeda' },
-                    { value: 'userID14', label: 'Pauline' },
-                    { value: 'userID15', label: 'Robert Wellington' },
-                    { value: 'userID16', label: 'Simon Dwyer' },
-                    { value: 'userID17', label: 'Sophie' },
-                    { value: 'userID18', label: 'Steve Christenson' },
-                    { value: 'userID19', label: 'Suzanne Thomson' },
-                    { value: 'userID20', label: 'Tyson Butler-Boschma' },
-                    { value: 'userID21', label: 'Teresa Fae' },
-                  ]}
-                />
+
+            <MultiSelect
+              multi
+              joinValues
+              delimiter={','}
+              name="facilitators"
+              value={selectedFacilitator}
+              onChange={this.facilitatorSelect}
+              required
+              options={users.map(user => {
+                return {value: user._id, label: `${user.f_name} ${user.l_name}`}
+              })}
+            /> 
              
               
               
@@ -258,18 +233,16 @@ class EventEdit extends Component {
               <input type="checkbox" defaultChecked={singleEvent.onsite ? true : false} name="onsite" />
             </div>
 
-            <SingleSelect
+            {/* <SingleSelect
           name="organisation"
           placeholder="Organisation"
           simpleValue
           value={selectedOrganisation}
           onChange={this.organisationSelect}
-          options={[
-            { value: 'Coder Academy', label: 'Coder Academy' },
-            { value: 'RedHill', label: 'Red Hill' },
-           
-            ]}
-          /><br/>
+          options={orgs.map(org => {
+            return {value: org._id, label: org.org_name}
+          })}
+          /><br/> */}
             <input type="text"  ref={this.notes} placeholder="Notes" defaultValue={singleEvent.notes} name="notes"/><br />
 
             <div className="dates">
